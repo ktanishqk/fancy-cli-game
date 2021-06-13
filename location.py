4b33b01aabe57498535c994b1afd413ab975d637
ktanishqk <68249492+ktanishqk@users.noreply.github.com>
2021-06-13 11:28:14 -0500

    initial commit with location support

---
+++ location.py
@@ -0,0 +1,13 @@
+class Location:
+    def __init__(self):
+        self.name = name
+        self.description= description
+        self.items = dict_items
+        self.directions = directions
+
+    def get_name(self):
+        return self.name
+    def get_description(self):
+        return self.description
+    def get_directions(self):
+        return self.directions     
