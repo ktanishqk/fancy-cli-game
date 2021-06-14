08d2dcce07775c621df06ee7cbb18040c504a16f
ktanishqk <68249492+ktanishqk@users.noreply.github.com>
2021-06-14 12:33:52 -0500

    Implemented directions for better functionality

--- cli.py
+++ cli.py
@@ -1,13 +1,21 @@
 from plumbum import cli
 import json
 from map_layout import MapLayout
+from game_engine import GameEngine
+
 
 class RPGame(cli.Application):
+    def print_name(self):
+        print(Figlet(font="slant")).renderText("ADVENTURE RPG")
+
     def main(self):
-        with open('map_json.json') as file:
+        with open("map_json.json") as file:
             data = json.load(file)
         map_layout = MapLayout(data)
+        game_engine = GameEngine(map_layout)
+        game_engine.examine_the_room()
+        # game_engine.start_game()
 
-        #file = json.loads('map_json.json', r)
+
 if __name__ == "__main__":
     RPGame()
--- directions.py
+++ directions.py
@@ -1,4 +1,10 @@
 class Directions:
     def __init__(self, direction: dict):
-        self.name = direction['directionName']
-        self.room = direction['room']
+        self.name = direction["directionName"]
+        self.room = direction["room"]
+
+    def get_name(self):
+        return self.name
+
+    def get_room(self):
+        return self.room
