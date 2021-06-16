5eb3831ee92de733bdcbc014836c7dc32163feca
ktanishqk <68249492+ktanishqk@users.noreply.github.com>
2021-06-15 23:16:55 -0500

    basic getter implementation

--- item.py
+++ item.py
@@ -6,3 +6,18 @@ class Item:
             self.damage = int(item.get("damage"))
             self.range = int(item.get("range"))
             self.accuracy = int(item.get("accuracy"))
+
+    def get_item_name(self):
+        return self.item_name
+
+    def get_description(self):
+        return self.description
+
+    def get_damage(self):
+        return self.damage
+
+    def get_range(self):
+        return self.range
+
+    def get_accuracy(self):
+        return self.accuracy
